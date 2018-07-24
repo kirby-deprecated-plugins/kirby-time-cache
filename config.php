8c120558c65fead246f87bff0fd432cfd6a472ee
Bruno Meilick <b@bnomei.com>
2018-07-24 10:52:21 +0200

    new cache rewrite
    Signed-off-by: Bruno Meilick <b@bnomei.com>

--- config.php
+++ config.php
@@ -2,11 +2,7 @@
     Kirby::plugin('jenstornell/time-cache', [
         'options' => [
             'pages' => false,
-            'expires' => 84400,
-            'filename.hash' => true,
-            'cache' => true,
-            'path' => function() {
-                return kirby()->roots()->storage() . '/cache-time';
-            },
+            'expires' => 84400, // seconds
+            'cache' => true
         ]
     ]);
